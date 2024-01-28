e4eccda16711c28573b5b24884464811c76cce9d
Rashed Rahimi <rashedrahimi01@gmail.com>
2024-01-28 16:00:04 +0430

    First commit for todo project

---
+++ lib/model/todo.dart
@@ -0,0 +1,34 @@
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+      ToDo(id: '01', todoText: 'Morning Excersize1', isDone: true),
+      ToDo(id: '02', todoText: 'Morning Excersize2', isDone: true),
+      ToDo(
+        id: '03',
+        todoText: 'Morning Excersize3',
+      ),
+      ToDo(
+        id: '04',
+        todoText: 'Morning Excersize4',
+      ),
+      ToDo(
+        id: '05',
+        todoText: 'Morning Excersize5',
+      ),
+      ToDo(
+        id: '06',
+        todoText: 'Morning Excersize6',
+      )
+    ];
+  }
+}
